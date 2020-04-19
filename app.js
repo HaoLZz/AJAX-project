8d5f62bcf71cf4817cbbe6e77c6a49f2a64a0b22
Haoliang Zhang <zhltom@126.com>
2020-04-19 17:53:09 +0800

    changed css

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Hello");
+
+document.getElementById('button').addEventListener('click', loadText);
+
+        function loadText(){
+            const xhr = new XMLHttpRequest();
+            console.log(xhr);
+            xhr.open('GET', 'CET-4.txt',true);
+            xhr.onload = function(){
+                if (this.status === 200) {
+                    console.log(this.responseText);
+                    document.getElementById('text').innerHTML = this.responseText;
+                }
+            }
+            // sends request
+            xhr.send();
+        }
